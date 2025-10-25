c39b27ba4bdb30f1aa081ca4d6cee4cf301b5104
Arpitpadhy <127716002+Arpitpadhy@users.noreply.github.com>
2025-10-25 11:06:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const statusElement = document.getElementById('status');
+const enterButton = document.getElementById('enterBtn');
+
+// Add click event listener to the button
+enterButton.addEventListener('click', () => {
+  statusElement.innerHTML = '<h1>Entered Metaverse</h1>';
+});
